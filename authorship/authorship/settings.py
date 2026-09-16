91f89002108f21e4643057418b6beba41487b538
vickkky3 <victoria.victorero@estudiante.uam.es>
2026-09-16 22:15:26 +0200

    actualizado settings

--- authorship/authorship/settings.py
+++ authorship/authorship/settings.py
@@ -70,10 +70,15 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://comfy-arte.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://comfyarte.onrender.com",
+    "https://comfy-arte.vercel.app",
+]
 
 ROOT_URLCONF = 'authorship.urls'
 
