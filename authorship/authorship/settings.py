9f6ad632cc60801cb82ba617ea347014ae26c371
vickkky3 <victoria.victorero@estudiante.uam.es>
2026-09-16 22:14:38 +0200

    actualizado settings

--- authorship/authorship/settings.py
+++ authorship/authorship/settings.py
@@ -32,7 +32,12 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'comfyarte.onrender.com',
+    '.onrender.com',
+]
 
 
 # Application definition
